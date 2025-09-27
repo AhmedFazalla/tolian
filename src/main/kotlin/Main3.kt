ef2ff72ca13cc1506b70bb16bd121901fc4c2a20
sirezo <joebertcerezo@gmail.com>
2025-09-27 13:13:05 +0800

    feat: add main function with string reversal and palindrome check

---
+++ src/main/kotlin/Main3.kt
@@ -0,0 +1,57 @@
+package org.example
+
+fun main() {
+  print("Input string: ")
+  val userInput = readln()
+
+  println("$userInput asdf ${userInput.reverse()}")
+
+  println(userInput)
+  println(reverseString(userInput))
+
+  if(userInput == reverseString(userInput)){
+    println("palindrome")
+  } else{
+    println("not palindrome")
+  }
+  val address = Address(street = "Tejero", streetNo = 123)
+  val user = User(name = "Joebert", age = 2, address).also{
+    println("also ni siya")
+  }
+  user.toKick()
+
+}
+
+fun reverseString(str: String): String = buildString {
+  for (i in str.lastIndex downTo 0) {
+    append(str[i])
+  }
+}
+
+// correct implementation of the `this`, which explicitly refers to the String class
+fun String.reverse(): String = buildString {
+  for (i in lastIndex downTo 0) {
+    append(this@reverse[i])
+  }
+}
+
+// this will return nothing, because the `this` refers to the buildString
+fun String.reverse1(): String = buildString {
+  for (i in lastIndex downTo 0) {
+    append(this[i])
+  }
+}
+
+class Address(street: String, streetNo: Int)
+class User(val name: String, age: Int, address: Address) {
+  init {
+    println("halaaaa")
+    require(age < 10) { "houyy bata ${age}" }
+  }
+  
+}
+
+fun User.toKick() {
+  println("Hello user $name")
+}
+
