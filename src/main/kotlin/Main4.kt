dbe887d84eb61a0f899742ff07d9f643d4925775
sirezo <joebertcerezo@gmail.com>
2025-09-27 18:40:03 +0800

    feat: implement main function and add string filtering functionality

--- src/main/kotlin/Main4.kt
+++ src/main/kotlin/Main4.kt
@@ -1,4 +1,30 @@
 package org.example
 
-class Main4 {
+import java.util.function.Predicate
+
+fun main() {
+  print("Input string: ")
+  val userInput = readln()
+
+  println(userInput.filtered { it.isLetter() })
+}
+
+fun String.filtered(predicate: (Char) -> Boolean): String {
+  val list = mutableListOf<Char>()
+  for(i in this) {
+    if(predicate(i)){
+      list.add(i)
+    }
+  }
+  return list.joinToString("")
+}
+
+fun <T> Iterable<T>.filtered(predicate: (T) -> Boolean): List<T> {
+  val list = mutableListOf<T>()
+  for(i in this) {
+    if(predicate(i)){
+      list.add(i)
+    }
+  }
+  return list
 }
